2ee22c8d490a5912086babdd028c4414a350dc5f
Duank <dediduank@gmail.com>
2023-01-03 23:44:27 +0700

    insert comment in code

--- src/main/java/classb/basiccrudapi/controller/myController.java
+++ src/main/java/classb/basiccrudapi/controller/myController.java
@@ -4,13 +4,13 @@
  */
 package classb.basiccrudapi.controller;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import model.entity.Barang;
 import model.jpacontroller.BarangJpaController;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,46 +20,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
  *
  * @author Duank
  */
 @RestController
+@CrossOrigin
 @ResponseBody
 @RequestMapping("/barang")
 
 public class myController {
 
-    Barang brg = new Barang();
-    BarangJpaController ctrl = new BarangJpaController();
+    Barang brg = new Barang(); // declare entity
+    BarangJpaController ctrl = new BarangJpaController(); // declare jpa controller
 
-    List<Barang> barangList = new ArrayList<Barang>();
+    List<Barang> barangList = new ArrayList<Barang>(); // declare new List variable
 
     @GetMapping()
     public List<Barang> viewAll() {
-        if (ctrl.findBarangEntities().isEmpty()) {
-            return List.of();
+        try {
+            return ctrl.findBarangEntities(); // return data if data is available
+        } catch (Exception e) {
+            return List.of(); // check data is empty or no
         }
-        return ctrl.findBarangEntities();
+
     }
 
     @GetMapping("/{id}")
     public List<Barang> viewDatabyId(@PathVariable("id") int id) {
-
-        brg = ctrl.findBarang(id);
-        barangList.clear(); // clear data agar tidak tampil lebih dari sekali
-        barangList.add(brg); // input data ke dalam list
-
-        return barangList;
+        try {
+            brg = ctrl.findBarang(id); // get data from entity
+            barangList.clear(); // clear data in list
+            barangList.add(brg); // fill list
+            return barangList; // show data
+        } catch (Exception e) {
+            return List.of(); // data is empty
+        }
     }
 
-    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String editData(@PathVariable("id") int id, @RequestBody Barang data ) {
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE) // convert string to JSON
+    public String editData(@PathVariable("id") int id, @RequestBody Barang data) {
         String rslt = "Data has been updated";
         try {
-            data.setId(id);
-            ctrl.edit(data);
+            data.setId(id); // inserting data
+            ctrl.edit(data); // updating data in entity
         } catch (Exception e) {
             rslt = e.toString() + " Update Failed";
         }
@@ -68,11 +72,11 @@ public class myController {
 
     }
 
-    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE) // convert string to json
     public String delete(@PathVariable("id") int id) {
         String rslt = "Data Berhasil Di Hapus";
         try {
-            ctrl.destroy(id);
+            ctrl.destroy(id); // delete data
         } catch (Exception e) {
             rslt = "Data Tidak Berhasil di hapus";
         }
